646ba8692bc36a3e0c2e71e8bdc75efedca605f4
azadbhandari15 <bhandariazad84@gmail.com>
2026-08-23 16:46:28 +0530

    Added Backend Skeleton Project

---
+++ mpl-backend/src/main/java/com/mpl/backend/MicroserviceForMplApplication.java
@@ -0,0 +1,13 @@
+package com.mpl.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceForMplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceForMplApplication.class, args);
+	}
+
+}
---
+++ mpl-backend/src/test/java/com/mpl/backend/MicroserviceForMplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceForMplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
